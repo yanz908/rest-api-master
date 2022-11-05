9b738dc363f606d0efe5cd9cd4bd7a91dd1f4275
yanz908 <91471034+yanz908@users.noreply.github.com>
2022-11-05 22:13:26 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,7 @@ var { fetchJson, runtime, getBuffer } = require('../lib/myfunc');
 
 // Apikey
 var listkey = ["danzz","9286c1a775","9267ic6a0f1","927j59de9c","921n567ea6","921h5a4282","925n2c494","928b0323c9","927b0k3hp7o2","925b04ib0j","023l1qhbpk","92b1a0h7ts","92a70b789c","9291a7bk0p1","92a7o8pe9c","92y1a7l0a6","9221a7i9h2","921a7k3n94","92a0kk2bc9","921a7l9pho2","92a2n1kb0j","92b0a75k6f","92u1a7pr8s"];
-var listkeyprem = ["danzz"];
+var listkeyprem = ["danzzprem"];
 
 
 // Features
